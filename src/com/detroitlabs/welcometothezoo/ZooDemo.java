a5442eb0b33b0de04334ff31a165eb2ad1178556
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-09-29 09:50:56 -0400

    Creating a main menu in ZooMaker class

--- src/com/detroitlabs/welcometothezoo/ZooDemo.java
+++ src/com/detroitlabs/welcometothezoo/ZooDemo.java
@@ -4,4 +4,21 @@ package com.detroitlabs.welcometothezoo;
  * Created by kyleofori on 9/29/14.
  */
 public class ZooDemo {
+    public static void main(String[] args) {
+
+        do {
+            ZooMaker.zooMenu();
+        } while (); //something isn't 8);
+
+
+//        Write a program that allows the zookeeper to:
+//        1.) Set up new Pens
+//        2.) Remove Pens
+//        3.) Add an Animal to a Pen
+//        4.) Add a BabyAnimal to a Pen
+//        5.) Remove Animals and BabyAnimals from pens
+//        6.) Display all the Animals in a Pen
+//        7.) Display all the Animals in the Zoo.
+        }
+    }
 }
---
+++ src/com/detroitlabs/welcometothezoo/ZooMaker.java
@@ -0,0 +1,18 @@
+package com.detroitlabs.welcometothezoo;
+
+/**
+ * Created by kyleofori on 9/29/14.
+ */
+public class ZooMaker {
+    public static void zooMenu() {
+        System.out.println("Welcome to the Royal Oak Zoo!\nHow would you like to start your adventure?");
+        System.out.println("1 - Set up new pens.");
+        System.out.println("2 - Remove pens.");
+        System.out.println("3 - Add an animal to a pen.");
+        System.out.println("4 - Add a baby animal to a pen.");
+        System.out.println("5 - Remove an animal or baby animal from a pen.");
+        System.out.println("6 - Display all the animals in a pen.");
+        System.out.println("7 - Display all the animals in the Royal Oak Zoo.");
+        System.out.println("8 - Leave the ROZ.");
+    }
+}

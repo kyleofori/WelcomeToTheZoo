b79f13ef58f7c14696151a64906a53560061ddb0
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-09-29 09:43:21 -0400

    Making the BabyAnimal class by extending Animal, adding an additional instance variable

--- src/com/detroitlabs/welcometothezoo/BabyAnimal.java
+++ src/com/detroitlabs/welcometothezoo/BabyAnimal.java
@@ -3,5 +3,14 @@ package com.detroitlabs.welcometothezoo;
 /**
  * Created by kyleofori on 9/29/14.
  */
-public class BabyAnimal {
+public class BabyAnimal extends Animal {
+    private double size;
+    private String gender;
+    private String species;
+    private String cuteness;
+
+    public BabyAnimal(double size, String gender, String species, String cuteness) {
+        super(size, gender, species);
+        this.cuteness = cuteness;
+    }
 }
